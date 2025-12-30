bbd72e9b34f8362ca1e1abe7d1b163aeaf863bcc
Tharunteja <pellakurutharunteja@gmail.com>
2025-12-30 19:19:37 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import SocialConnect from './components/SocialConnect';
-import SocialOptimized from './components/SeoOptimized';
 
 function App() {
   return (
     <div className="App">
       <SocialConnect />
-      <SocialOptimized />
     </div>
   );
 }
